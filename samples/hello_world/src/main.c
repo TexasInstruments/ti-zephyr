7e0e559157f78b483d05053a3a8c8041c32dcb00
Vivian Pais <v-pais@ti.com>
2026-07-31 13:13:48 +0530

    samples: hello_world: add lp_mspm0g3519 board-support sample
    Add samples/hello_world as the initial build and board-support
    verification sample. Confirms the manifest, module registration,
    and lp_mspm0g3519 board support resolve correctly end-to-end.
    
    src/main.c and CMakeLists.txt are original to this repository
    (BSD-3-Clause, Texas Instruments Incorporated).
    
    Signed-off-by: Vivian Pais <v-pais@ti.com>

---
+++ samples/hello_world/src/main.c
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2026 Texas Instruments Incorporated
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Hello World! %s\n", CONFIG_BOARD_TARGET);
+
+	return 0;
+}
